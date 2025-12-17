bc14d11201ebf6135ed4a9922c6b10628e8e4dc9
이성수 <sslee@wylie.co.kr>
2025-12-17 15:39:59 +0900

    공통컴포넌트 테스트

---
+++ layout/header.js
@@ -0,0 +1,14 @@
+customElements.define(
+  "app-header",
+  class extends HTMLElement {
+    connectedCallback() {
+      const title = this.getAttribute("title") ?? "";
+
+      this.innerHTML = `
+        <header class="header_wrap">
+          <h1>${title}</h1>
+        </header>
+      `;
+    }
+  }
+);
